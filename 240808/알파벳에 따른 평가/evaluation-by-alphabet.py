8226cc88b0b1adbcaf047909096cd85193e81c08
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-08 14:55:06 +0900

    Title: 알파벳에 따른 평가, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240808/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+alp = input()
+
+if alp == 'S':
+    print("Superior")
+elif alp == 'A':
+    print("Excellent")
+elif alp == 'B':
+    print("Good")
+elif alp == 'C':
+    print("Usually")
+elif alp == 'D':
+    print("Effort")
+else : 
+    print("Failure")
