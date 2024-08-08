f3bb743a37683ddb4e04eb3395afa07fe97a8746
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-08 14:58:44 +0900

    Title: 살 수 있는 물건 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 240808/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -2,9 +2,9 @@ money = int(input())
 
 if money >=3000:
     print("book")
-elif 1000>= money >3000:
+elif 3000> money >=1000:
     print("mask")
-elif 500>= money >1000:
+elif 1000> money >=500:
     print("pen")
 else:
     print("no")
