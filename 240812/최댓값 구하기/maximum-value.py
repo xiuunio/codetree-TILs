82bd130fb201102d8a0a4a1ad22fe35e077593d3
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-12 23:33:40 +0900

    Title: 최댓값 구하기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/최댓값 구하기/maximum-value.py
+++ 240812/최댓값 구하기/maximum-value.py
@@ -2,4 +2,13 @@ inp = input()
 arr = inp.split()
 a,b,c = int(arr[0]),int(arr[1]),int(arr[2])
 
-print(max(a,b,c))
+if a>=b :
+    if a>=c:
+        print(a)
+    else: 
+        print(c)
+else:
+    if b>=c:
+        print(b)
+    else:
+        print(c)
