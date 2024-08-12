2e72a6cb94170bfa43d3786d8d9566ec486dee07
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-12 16:01:31 +0900

    Title: 4번의 크기 비교, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240812/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+inp = input()
+arr = inp.split()
+
+b = int(arr[0])
+c = int(arr[1])
+d = int(arr[2])
+e = int(arr[3])
+
+print(int(a>b))
+print(int(a>c))
+print(int(a>d))
+print(int(a>e))
