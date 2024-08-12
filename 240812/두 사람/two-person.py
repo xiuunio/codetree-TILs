104bf51286e177b37d2824cb75326233aa94c7a4
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-12 17:12:05 +0900

    Title: 두 사람, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/두 사람/two-person.py
+++ 240812/두 사람/two-person.py
@@ -7,15 +7,5 @@ brr = inpb.split()
 aa,ass = int(arr[0]),arr[1]
 ba,bss = int(brr[0]), brr[1]
 
-if aa>=19 or ba>=19:
-    if bss =="M":
-        print("1")
-    elif ass == "M":
-        print("1")
-elif bss == "M" or ass == "M":
-    if aa >=19 and ba <19:
-        print("0")
-    elif aa<19 and ba>=19:
-        print("0")
-else:
-    print("0")
+if (aa>=19 and ass == "M") or (ba >=19 and bss == "M"):
+    print("1")
