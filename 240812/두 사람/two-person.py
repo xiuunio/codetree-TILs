a9291cb7668404af0e81ff1277c18c42730adac0
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-12 17:10:11 +0900

    Title: 두 사람, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/두 사람/two-person.py
+++ 240812/두 사람/two-person.py
@@ -12,5 +12,10 @@ if aa>=19 or ba>=19:
         print("1")
     elif ass == "M":
         print("1")
+elif bss == "M" or ass == "M":
+    if aa >=19 and ba <19:
+        print("0")
+    elif aa<19 and ba>=19:
+        print("0")
 else:
     print("0")
