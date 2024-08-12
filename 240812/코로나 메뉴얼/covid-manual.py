e32041c4567135c6904efc20d82ec18888e8920b
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-12 23:41:31 +0900

    Title: 코로나 메뉴얼, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240812/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,22 @@
+inp1 = input()
+inp2 = input()
+inp3 = input()
+
+arr1 = inp1.split()
+arr2 = inp2.split()
+arr3 = inp3.split()
+
+aa, ac = arr1[0], int(arr1[1])
+ba, bc = arr2[0], int(arr2[1])
+ca, cc = arr3[0], int(arr3[1])
+
+if (aa =="Y" and ac>=37):
+    if (ba =="Y" and bc>=37)or(ca =="Y" and cc>=37):
+        print("E")
+    else:
+        print("N")
+elif (ba =="Y" and bc>=37):
+    if (aa =="Y" and ac>=37)or(ca =="Y" and cc>=37):
+        print("E")
+    else:
+        print("N")
