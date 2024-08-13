55bde1e05607f220abda5410225e79b7faad2be8
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-13 22:31:22 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/369 게임/369-game.py
+++ 240813/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1,n+1):
-    if (i%10!=0 and (i%10)%3==0) or i%3==0 or (i>=30 and i//30<10):
+    if (i%10!=0 and (i%10)%3==0) or i%3==0 or (i>=30 and i%30<10):
         print("0",end=' ')
     else:
         print(i,end=' ')
