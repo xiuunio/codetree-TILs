000df6ec6e3d2d72a35ec502429d4d1e3fa6cfc8
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-15 11:00:30 +0900

    Title: 3n + 1 수열, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240815/3n + 1 수열/3n-plus-1-sequence.py
+++ 240815/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,11 +1,13 @@
 n=int(input())
 cnt=0
 while True:
-    if n%2==0:
-        n/=2
-    else:
-        n=n*3+1
-    cnt+=1
     if n==1:
         print(cnt)
         break
+    if n%2==0:
+        n/=2
+        cnt+=1
+
+    else:
+        n=n*3+1
+        cnt+=1
