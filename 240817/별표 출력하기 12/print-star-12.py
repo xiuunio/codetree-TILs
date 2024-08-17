47442bcaec1b86cc120c3ed2bee43536f72b06dd
xiuunio <161451092+xiuunio@users.noreply.github.com>
2024-08-17 16:38:30 +0900

    Title: 별표 출력하기 12, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240817/별표 출력하기 12/print-star-12.py
+++ 240817/별표 출력하기 12/print-star-12.py
@@ -5,13 +5,13 @@ for i in range(n):
     for j in range(n):
         if j %2 ==0:
             if i == 0:
-                print('*',end='')
+                print('*',end=' ')
             else:
                 print(' ',end='')
             
         else:
             if i<=j:
-                print('*',end='')
+                print('*',end=' ')
             else:
                 print(' ',end='')
     print()
